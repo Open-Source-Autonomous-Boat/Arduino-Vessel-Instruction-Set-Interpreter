b675e0cedeef8443a6b0a2fe7a594ee553983c65
Michael MacDonald <34051815+Michael2MacDonald@users.noreply.github.com>
2021-06-18 12:08:00 -0700

    Update and rename VISI.h to VISInterpreter.h

--- src/VISI.h
+++
@@ -1,7 +0,0 @@
-#ifndef _VISI_H /* start of the GPSTools.h */
-#define _VISI_H
-
-double CalcHeading(double lat1, double long1, double lat2, double long2);
-double CalcDistance(double lat1, double long1, double lat2, double long2);
-
-#endif /* _GPSTools_H */
---
+++ src/VISInterpreter.h
@@ -0,0 +1,6 @@
+#ifndef _VIS-Interpreter_H /* start of the VIS-Interpreter.h */
+#define _VIS-Interpreter_H
+
+
+
+#endif /* _VIS-Interpreter_H */
