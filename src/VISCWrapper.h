8820c4aaa39009375c186d9f95cb1dd0823b952b
Some_Indonesian <troy.dwijanto2@gmail.com>
2021-07-27 13:07:02 -0400

    Fixed C compatibility

--- src/VISCWrapper.h
+++ src/VISCWrapper.h
@@ -1,6 +1,7 @@
 #ifndef VIS_CWRAPPER_H
 #define VIS_CWRAPPER_H
 #include <string.h>
+
 #include "VISParser.h"
 
 /* Gotta make it C89 compat */
@@ -25,44 +26,46 @@
 
 #ifdef __cplusplus
 extern "C" {
+#endif
 /* Declare a struct for C to use */
 
 /* Gets parser class as struct */
-extern VISParser *getVISParser();
+VISParser *getVISParser();
 
 /*
  * Gets file class as struct
  * @param[in] a_path: Path to file
  */
-extern VISFile *getVISFile(char* a_path);
+VISFile *getVISFile(char *a_path);
 /*
  * C wrapper to VISParser::OpenFile(std::string path)
  * @param[in,out] a_parser: The C compat(?) struct
  * @param[in] a_path: The path to file
  */
-extern void VISParserOpenFile(VISParser *a_parser, char *a_path);
+void VISParserOpenFile(VISParser *a_parser, char *a_path);
 /*
  * C wrapper to VISParser::CloseFile()
  * @param[in] a_parser: The C compat(?) struct
  */
-extern void VISParserCloseFile(VISParser *a_parser);
+void VISParserCloseFile(VISParser *a_parser);
 /*
  * C function to delete struct/class
  * @param[in] a_parser: The C compat(?) struct
  */
-extern void delVISParser(VISParser *a_parser);
+void delVISParser(VISParser *a_parser);
 
 /*
  * Gets file object, C can do this right?
  * @param[in] a_file: The C compat(?) struct
  */
-extern void* VISFileGetObject(VISFile* a_file);
+void *VISFileGetObject(VISFile *a_file);
 
 /*
  * Gets file path
  * @param[in] a_file: The C compat(?) struct
  */
-extern char* VISFileGetPath(VISFile* a_file);
+char *VISFileGetPath(VISFile *a_file);
+#ifdef __cplusplus
 }
 #endif
 #endif /* VIS_CWRAPPER_H */
